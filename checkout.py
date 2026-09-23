6061b847fedf7e54ac25b6daefa7809e14706332
David-Chri <dapach@umich.edu>
2026-09-22 20:09:02 -0400

    Checkout.py code updated

--- checkout.py
+++ checkout.py
@@ -1,6 +1,6 @@
 import random
-Cash = 150
+Cash = random.randint(100, 300)
 Items = random.randint(1, 100)
-total = Items - Cash
+total = Cash - Items
 print(total)
 # Unsure what you mean by investagation so I'm going this route
